eb0b81cfda15d63e6e318ca658b620f95eb2d413
MieAh <100568933+MieAh@users.noreply.github.com>
2023-08-24 23:02:20 +0800

    feat(internal/pkg/common/interceptor/rpc):添加grpc日志拦截器

---
+++ internal/pkg/common/interceptor/rpc/logger_interceptor.go
@@ -0,0 +1,28 @@
+package rpc
+
+import (
+	"context"
+	"topview-ttk/internal/pkg/common/ttkerr"
+
+	"github.com/pkg/errors"
+	"github.com/zeromicro/go-zero/core/logx"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func LoggerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	resp, err = handler(ctx, req)
+	if err != nil {
+		causeErr := errors.Cause(err)
+		if e, ok := causeErr.(*ttkerr.CodeError); ok {
+
+			//convert grpc err
+			err = status.Error(codes.Code(e.GetErrCode()), e.GetErrMsg())
+		} else {
+			logx.WithContext(ctx).Errorf("【RPC-SRV-ERR】 %+v", err)
+		}
+	}
+	return resp, err
+}
